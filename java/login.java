1764a7f5f1c96130814f0f403a3851361dac9cb9
PAUL KAHENYA <64004932+KahenyaPaul@users.noreply.github.com>
2022-04-17 22:53:01 +0300

    Love doing this

--- java/login.java
+++ java/login.java
@@ -1,8 +1,19 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
 
 public class login {
 
     public static void main(String[] args) {
-        
+
+        int age = 30;
+        String name ="Paul Kahenya";
+        if(age>20){
+
+            System.out.println(name);
+        }
+        else{
+
+            System.out.println("Thats not my age");
+        }
     }
     
 }
