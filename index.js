e224dc8be43d0ca14bd9260c9f2e530eb9a108a6
PAUL KAHENYA <64004932+KahenyaPaul@users.noreply.github.com>
2022-04-17 22:38:39 +0300

    I love doing this

---
+++ index.js
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+    <h1 id="demo"> Learning to code with javascript</h1>
+    <script type ="text/javascript">
+
+        document.getElementById("demo").innerHTML=Date();
+
+    
+
+
+
+
+
+    </script>
+</body>
+
+
+</html>
