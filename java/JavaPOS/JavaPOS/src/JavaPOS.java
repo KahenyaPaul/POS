9050d5a3590cf24da164f86e3f5b2e00d10ea530
PAUL KAHENYA <64004932+KahenyaPaul@users.noreply.github.com>
2022-04-18 14:11:14 +0300

    Learning on how to create reall apps'

---
+++ java/JavaPOS/JavaPOS/src/JavaPOS.java
@@ -0,0 +1,208 @@
+
+public class JavaPOS extends javax.swing.JFrame {
+
+   
+    public JavaPOS() {
+        initComponents();
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jButton14 = new javax.swing.JButton();
+        jButton30 = new javax.swing.JButton();
+        jButton31 = new javax.swing.JButton();
+        jButton32 = new javax.swing.JButton();
+        jButton33 = new javax.swing.JButton();
+        jButton34 = new javax.swing.JButton();
+        jButton35 = new javax.swing.JButton();
+        jButton36 = new javax.swing.JButton();
+        jButton37 = new javax.swing.JButton();
+        jButton38 = new javax.swing.JButton();
+        jButton39 = new javax.swing.JButton();
+        jButton40 = new javax.swing.JButton();
+        jButton41 = new javax.swing.JButton();
+        jButton42 = new javax.swing.JButton();
+        jButton43 = new javax.swing.JButton();
+        jButton44 = new javax.swing.JButton();
+        jButton45 = new javax.swing.JButton();
+        jButton46 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(null));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton14.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton14, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 10, 120, 110));
+
+        jButton30.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton30, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 120, 110));
+
+        jButton31.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton31, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 10, 120, 110));
+
+        jButton32.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton32, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 10, 120, 110));
+
+        jButton33.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton33, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 10, 130, 110));
+
+        jButton34.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton34, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 10, 120, 110));
+
+        jButton35.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton35, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 150, 120, 110));
+
+        jButton36.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton36, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 150, 120, 110));
+
+        jButton37.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton37, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 150, 120, 110));
+
+        jButton38.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton38, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 150, 130, 110));
+
+        jButton39.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton39, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 150, 120, 110));
+
+        jButton40.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton40, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 150, 120, 110));
+
+        jButton41.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton41, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 310, 120, 110));
+
+        jButton42.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton42, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 310, 120, 110));
+
+        jButton43.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton43, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 310, 120, 110));
+
+        jButton44.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton44, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 310, 130, 110));
+
+        jButton45.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton45, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 310, 120, 110));
+
+        jButton46.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jPanel1.add(jButton46, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 310, 120, 110));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 0, 800, 430));
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createBevelBorder(null));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton1.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jButton1.setText("7");
+        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 10, 80, 80));
+
+        jButton2.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jButton2.setText("7");
+        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 10, 80, 80));
+
+        jButton3.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jButton3.setText("7");
+        jPanel2.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 80, 80));
+
+        jButton4.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jButton4.setText("7");
+        jPanel2.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 100, 80, 80));
+
+        jButton5.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jButton5.setText("7");
+        jPanel2.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 100, 80, 80));
+
+        jButton6.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jButton6.setText("7");
+        jPanel2.add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 100, 80, 80));
+
+        jButton7.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jButton7.setText("7");
+        jPanel2.add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 190, 80, 80));
+
+        jButton8.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jButton8.setText("7");
+        jPanel2.add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 190, 80, 80));
+
+        jButton9.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jButton9.setText("7");
+        jPanel2.add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 190, 80, 80));
+
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 290, 410));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Item", "Qty", "Amount"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 10, 350, 410));
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+   
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JavaPOS().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton14;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton30;
+    private javax.swing.JButton jButton31;
+    private javax.swing.JButton jButton32;
+    private javax.swing.JButton jButton33;
+    private javax.swing.JButton jButton34;
+    private javax.swing.JButton jButton35;
+    private javax.swing.JButton jButton36;
+    private javax.swing.JButton jButton37;
+    private javax.swing.JButton jButton38;
+    private javax.swing.JButton jButton39;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton40;
+    private javax.swing.JButton jButton41;
+    private javax.swing.JButton jButton42;
+    private javax.swing.JButton jButton43;
+    private javax.swing.JButton jButton44;
+    private javax.swing.JButton jButton45;
+    private javax.swing.JButton jButton46;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ java/beginnersTutorials/src/main/java/com/mycompany/beginnerstutorials/BeginnersTutorials.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.beginnerstutorials;
+
+
+public class BeginnersTutorials {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
